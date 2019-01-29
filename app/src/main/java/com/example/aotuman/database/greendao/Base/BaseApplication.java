6355f76df924473c5afbaeafb33dc03666b3fc38
huangkunting <huangkunting@wxchina.com>
2019-01-29 17:35:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aotuman/database/greendao/Base/BaseApplication.java
@@ -0,0 +1,36 @@
+package com.example.aotuman.database.greendao.Base;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.aotuman.database.greendao.Bean.DaoMaster;
+import com.example.aotuman.database.greendao.Bean.DaoSession;
+import com.example.common.app.BaseApp;
+import com.facebook.stetho.Stetho;
+
+
+public class BaseApplication extends BaseApp {
+
+    private static DaoSession daoSession;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+        //配置数据库
+        setupDatabase();
+    }
+
+    /**
+     * 配置数据库
+     */
+    private void setupDatabase() {
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "shop.db", null);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        DaoMaster daoMaster = new DaoMaster(db);
+        daoSession = daoMaster.newSession();
+    }
+
+    public static DaoSession getDaoInstant() {
+        return daoSession;
+    }
+}
---
+++ app/src/main/java/com/example/aotuman/database/greendao/Dao/LoveDao.java
@@ -0,0 +1,50 @@
+package com.example.aotuman.database.greendao.Dao;
+
+
+import com.example.aotuman.database.greendao.Base.BaseApplication;
+import com.example.aotuman.database.greendao.Bean.Shop;
+import com.example.aotuman.database.greendao.Bean.ShopDao;
+
+import java.util.List;
+
+/**
+ * Created by aotuman on 2018/4/19.
+ */
+public class LoveDao {
+
+    /**
+     * 添加数据
+     *
+     * @param shop
+     */
+    public static void insertLove(Shop shop) {
+        BaseApplication.getDaoInstant().getShopDao().insert(shop);
+    }
+
+    /**
+     * 删除数据
+     *
+     * @param id
+     */
+    public static void deleteLove(long id) {
+        BaseApplication.getDaoInstant().getShopDao().deleteByKey(id);
+    }
+
+    /**
+     * 更新数据
+     *
+     * @param shop
+     */
+    public static void updateLove(Shop shop) {
+        BaseApplication.getDaoInstant().getShopDao().update(shop);
+    }
+
+    /**
+     * 查询条件为Type=TYPE_LOVE的数据
+     *
+     * @return
+     */
+    public static List<Shop> queryLove() {
+        return BaseApplication.getDaoInstant().getShopDao().queryBuilder().where(ShopDao.Properties.Type.eq(Shop.TYPE_LOVE)).list();
+    }
+}
